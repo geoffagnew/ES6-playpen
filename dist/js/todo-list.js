b84f230a4d267ea89bf275b279ca0ec3d6648524
Geoff Agnew <gagnew@gmail.com>
2017-12-23 13:27:46 -0500

    Started building HTML5 todo list.

---
+++ dist/js/todo-list.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var theList = document.getElementById('todo-list');
+var saveAll = document.getElementById('save-all');
+var clearAll = document.getElementById('clear-all');
---
+++ src/js/todo-list.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const theList = document.getElementById('todo-list');
+const saveAll = document.getElementById('save-all');
+const clearAll = document.getElementById('clear-all');
+
