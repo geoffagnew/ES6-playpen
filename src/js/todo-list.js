2c881f97deaae0f76a486ef7b9d1925fa5741176
Geoff Agnew <gagnew@gmail.com>
2017-12-23 14:09:41 -0500

    Completed basic HTML5 todo list.

--- src/js/todo-list.js
+++ src/js/todo-list.js
@@ -1,6 +1,33 @@
 'use strict';
 
+/* global localStorage */
+
 const theList = document.getElementById('todo-list');
 const saveAll = document.getElementById('save-all');
 const clearAll = document.getElementById('clear-all');
+
+// function to check if existing data has been saved in localStorage from previous visits
+function loadToDo() {
+  if (localStorage.getItem('savedList')) {
+    theList.innerHTML = localStorage.getItem('savedList');
+  }
+}
+
+// call the function to check if existing data has been saved
+loadToDo();
+
+// event listener for the saveall button
+saveAll.addEventListener('click', function(e) {
+  e.preventDefault();
+  localStorage.setItem('savedList', theList.innerHTML);
+  console.log('Save event has fired.');
+});
+
+// event listener for the clear all button
+clearAll.addEventListener('click', function(e) {
+  e.preventDefault();
+  localStorage.clear();
+  theList.innerHTML = '<li>' + 'Add your first item' + '</li>';
+  console.log('Clear event has fired.');
+});
 
