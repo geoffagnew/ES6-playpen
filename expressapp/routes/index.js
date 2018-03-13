b19344ebfccf3c7a3ed3b1c4e02f45452e77e14a
Geoff Agnew <gagnew@gmail.com>
2018-03-12 22:25:00 -0400

    Building out views

--- expressapp/routes/index.js
+++ expressapp/routes/index.js
@@ -3,34 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var starters = '';
-  var benchMob = '';
-  var playerData = req.app.get('appData');
-  playerData.roster.forEach(function(item, i) {
-    if (i <= 4) {
-      starters += `
-        <li>
-          <strong>${item.name}</strong>
-          ${item.position}
-        </li>
-      `;
-    } else {
-      benchMob += `
-        <li>
-          <strong>${item.name}</strong>
-          ${item.position}
-        </li>
-      `;
-    }
-  })
-  res.send(`
-    <h1>Toronto Raptors 2018 roster</h1>
-    <h2>Starters</h2>
-    ${starters}
-    <h2>Bench mob</h2>
-    ${benchMob}
-  `);
-  // res.render('index', { title: 'Express' });
+  // var starters = '';
+  // var benchMob = '';
+  // var playerData = req.app.get('appData');
+  // playerData.roster.forEach(function(item, i) {
+  //   if (i <= 4) {
+  //     starters += `
+  //       <li>
+  //         <strong>${item.name}</strong>
+  //         ${item.position}
+  //       </li>
+  //     `;
+  //   } else {
+  //     benchMob += `
+  //       <li>
+  //         <strong>${item.name}</strong>
+  //         ${item.position}
+  //       </li>
+  //     `;
+  //   }
+  // })
+  // res.send(`
+  //   <h1>Toronto Raptors 2018 roster</h1>
+  //   <h2>Starters</h2>
+  //   ${starters}
+  //   <h2>Bench mob</h2>
+  //   ${benchMob}
+  // `);
+  var myName = 'Bonestorm';
+  res.render('index', {
+    title: 'Express',
+    name: myName
+  });
 });
 
 module.exports = router;
