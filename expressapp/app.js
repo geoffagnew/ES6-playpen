1216b5416df3143ddef5ac0447d2d1f3adc1154c
Geoff Agnew <gagnew@gmail.com>
2018-03-12 21:09:29 -0400

    Added loop to extract json data from players list.

--- expressapp/app.js
+++ expressapp/app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// json data
+var playerData = require('./data/raptors.json');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -13,6 +16,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// make player data available to the app
+app.set('appData', playerData);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- expressapp/routes/index.js
+++ expressapp/routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var starters = '';
+  var benchMob = '';
+  var playerData = req.app.get('appData');
+  playerData.roster.forEach(function(item, i) {
+    if (i <= 4) {
+      starters += `
+        <li>
+          <strong>${item.name}</strong>
+          ${item.position}
+        </li>
+      `;
+    } else {
+      benchMob += `
+        <li>
+          <strong>${item.name}</strong>
+          ${item.position}
+        </li>
+      `;
+    }
+  })
+  res.send(`
+    <h1>Toronto Raptors 2018 roster</h1>
+    <h2>Starters</h2>
+    ${starters}
+    <h2>Bench mob</h2>
+    ${benchMob}
+  `);
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
